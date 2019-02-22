83bf836864d226528bd6c49a64fe3fee908f97bc
Andrew Johnson <ajohnson.uva@gmail.com>
2019-02-22 00:07:23 -0500

    edit ingredient css selector

--- app/jobs/drink_scraper_job.rb
+++ app/jobs/drink_scraper_job.rb
@@ -22,11 +22,12 @@ class DrinkScraperJob
       if doc.css("table.infobox").size > 1
 
         doc.css("table.infobox").select {|node| node.text.match?("IBA official") }.each.with_index do |node, index|
+          # binding.pry if node.css("tr").find {|inner_node| inner_node.css("th").text.match?("IBA specified") }.css("td ul li").map {|inner_node| inner_node.text}.empty?
           name = node.css("caption").children.text
           hash[name] ||= {
             'image' => "https:" + (node.css("a.image img").select {|inner_node| inner_node.attributes['width'].value.to_i > 100}.first&.attributes&.[]('src')&.value || doc.css("a.image img").select {|node| node.attributes['width'].value.to_i > 100}.first&.attributes&.[]('src')&.value || DEFAULT_COCKTAIL_URL),
             'primary_alcohols' => node.css("tr").find {|node| node.css("th").text.match?("Primary alcohol") }.css("li a").map {|node| node.text},
-            'ingredients' => node.css("tr").find {|inner_node| inner_node.css("th").text.match?("IBA specified") }.css("td ul li").map {|inner_node| inner_node.text},
+            'ingredients' => node.css("tr").find {|inner_node| inner_node.css("th").text.match?("ingredients") }.css("td ul li").map {|inner_node| inner_node.text},
             'preparation' => node.css("tr").find {|inner_node| inner_node.css("th").text.match?("Preparation") }.css("td").first.text
           }
           puts hash.keys.last
@@ -39,11 +40,12 @@ class DrinkScraperJob
 
       else
         name = doc.css("table.infobox caption").first.children.text
+        # binding.pry if inner_doc.css("tr").find {|node| node.css("th").text.match?("IBA") }.css("td ul li").map {|node| node.text}.empty?
         # binding.pry unless doc.css("a.image img").select {|node| node.attributes['width'].value.to_i > 100}.first&.attributes&.[]('src')&.value
         hash[name] ||= {
           'image' => "https:" + (doc.css("a.image img").select {|node| node.attributes['width'].value.to_i > 100}.first&.attributes&.[]('src')&.value || DEFAULT_COCKTAIL_URL),
           'primary_alcohols' => inner_doc.css("tr").find {|node| node.css("th").text.match?("Primary alcohol") }.css("li a").map {|node| node.text},
-          'ingredients' => inner_doc.css("tr").find {|node| node.css("th").text.match?("IBA") }.css("td ul li").map {|node| node.text},
+          'ingredients' => inner_doc.css("tr").find {|node| node.css("th").text.match?("ingredients") }.css("td ul li").map {|node| node.text},
           'preparation' => inner_doc.css("tr").find {|node| node.css("th").text.match?("Preparation") }.css("td").first.text
         } unless name.match?("colada")
         puts hash.keys.last
