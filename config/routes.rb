ae4104714322079264637e8daf3442e65a25ada4
Andrew Johnson <ajohnson.uva@gmail.com>
2019-02-22 15:32:02 -0500

    add admin routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :drinks
+  resources :drinks, only: %i(index show)
   devise_for :users
+  resources :admin do
+    resources :drinks
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
