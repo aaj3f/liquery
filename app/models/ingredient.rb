c9420c7459b694be0ce4c250a460f274b7f254a3
Andrew Johnson <ajohnson.uva@gmail.com>
2019-02-23 23:41:03 -0500

    add links to new and show templates in drinks/views

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,4 +2,8 @@ class Ingredient < ApplicationRecord
   belongs_to :flavor_profile, optional: true
   has_many :measures
   has_many :drinks, through: :measures
+
+  def name_with_formatting
+    self.name
+  end
 end
