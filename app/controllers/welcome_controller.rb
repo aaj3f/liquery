8b97d2bbd0663d8960ca5219913350a8f8322e2c
Andrew Johnson <ajohnson.uva@gmail.com>
2019-02-25 19:15:41 -0500

    edit authorizations for logged_in user

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,11 @@
 class WelcomeController < ApplicationController
 
   def index
-    
+    if user_signed_in? && current_user.ratings.empty?
+      redirect_to quiz_question_one_path
+    elsif user_signed_in?
+      redirect_to drinks_path
+    end
   end
 
 end
