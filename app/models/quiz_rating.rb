8e5ffe4cca598642da89690d0b3b7ab33035a173
Andrew Johnson <ajohnson.uva@gmail.com>
2019-03-06 09:08:32 -0500

    remove redundant validation

--- app/models/quiz_rating.rb
+++ app/models/quiz_rating.rb
@@ -1,6 +1,4 @@
 class QuizRating < ApplicationRecord
   belongs_to :drink
   belongs_to :quiz
-
-  validates :score, inclusion: { in: %w(nil 1 -1) }
 end
--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,6 +1,4 @@
 class Rating < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :drink
-
-  validates :score, inclusion: { in: %w(nil 1 -1) }
 end
