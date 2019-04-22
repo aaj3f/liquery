4e7687ac419a5adbc84448849b344598fd76816d
Andrew Johnson <ajohnson.uva@gmail.com>
2019-04-22 19:21:21 -0400

    escape helper html

--- app/helpers/quiz_helper.rb
+++ app/helpers/quiz_helper.rb
@@ -2,17 +2,17 @@ module QuizHelper
 
   def modal_header
     if current_user.ratings.present?
-      "Yasss boo!!<br>Welcome back!!"
+      CGI::escapeHTML('Yasss boo!!<br>Welcome back!!')
     else
-      "Heyo sis!<br>You look thirsty!"
+      CGI::escapeHTML('Heyo sis!<br>You look thirsty!')
     end
   end
 
   def modal_text
     if current_user.ratings.present?
-      "We already have a sense for some of the drinks you tend to like.<br>But if you wanna give us some new information or update your likes and dislikes<br>go ahead bebe!!"
+      CGI::escapeHTML('We already have a sense for some of the drinks you tend to like.<br>But if you wanna give us some new information or update your likes and dislikes<br>go ahead bebe!!')
     else
-      "We're gonna get you a cocktail pronto,<br>we just need to ask a few questions first<br>so we can figure your drink out."
+      CGI::escapeHTML('We\'re gonna get you a cocktail pronto,<br>we just need to ask a few questions first<br>so we can figure your drink out.')
     end
   end
 
