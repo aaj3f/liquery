dbe4d0300a4ea9505cd2e20a05bd8b195f657f60
Andrew Johnson <ajohnson.uva@gmail.com>
2019-04-22 13:04:58 -0400

    reset database to fix corruption

--- db/schema.rb
+++ db/schema.rb
@@ -16,90 +16,92 @@ ActiveRecord::Schema.define(version: 2019_03_01_145005) do
   enable_extension "plpgsql"
 
   create_table "drinks", force: :cascade do |t|
-    t.text "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.text "preparation"
-    t.text "image"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "preparation"
+    t.string "image"
     t.boolean "test_drink", default: false
   end
 
   create_table "flavor_profiles", force: :cascade do |t|
-    t.text "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "ingredients", force: :cascade do |t|
-    t.text "name"
+    t.string "name"
     t.bigint "flavor_profile_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.index ["flavor_profile_id"], name: "idx_16429_index_ingredients_on_flavor_profile_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["flavor_profile_id"], name: "index_ingredients_on_flavor_profile_id"
   end
 
-  create_table "measures", id: :bigint, default: nil, force: :cascade do |t|
+  create_table "measures", force: :cascade do |t|
     t.bigint "drink_id"
     t.bigint "ingredient_id"
     t.float "size"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.text "measurement_type"
-    t.index ["drink_id"], name: "idx_16445_index_measures_on_drink_id"
-    t.index ["ingredient_id"], name: "idx_16445_index_measures_on_ingredient_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "measurement_type"
+    t.index ["drink_id"], name: "index_measures_on_drink_id"
+    t.index ["ingredient_id"], name: "index_measures_on_ingredient_id"
   end
 
   create_table "quiz_ratings", force: :cascade do |t|
     t.bigint "drink_id"
     t.bigint "quiz_id"
-    t.bigint "score"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.index ["drink_id"], name: "idx_16460_index_quiz_ratings_on_drink_id"
-    t.index ["quiz_id"], name: "idx_16460_index_quiz_ratings_on_quiz_id"
+    t.integer "score"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["drink_id"], name: "index_quiz_ratings_on_drink_id"
+    t.index ["quiz_id"], name: "index_quiz_ratings_on_quiz_id"
   end
 
   create_table "quizzes", force: :cascade do |t|
     t.bigint "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.bigint "flavor_profile_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "flavor_profile_id"
     t.boolean "use_previous_ratings", default: false
-    t.bigint "recommendation"
-    t.index ["user_id"], name: "idx_16453_index_quizzes_on_user_id"
+    t.integer "recommendation"
+    t.index ["user_id"], name: "index_quizzes_on_user_id"
   end
 
   create_table "ratings", force: :cascade do |t|
     t.bigint "user_id"
     t.bigint "drink_id"
-    t.bigint "score"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.integer "score"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.boolean "recommended", default: false
-    t.index ["drink_id"], name: "idx_16413_index_ratings_on_drink_id"
-    t.index ["user_id"], name: "idx_16413_index_ratings_on_user_id"
+    t.index ["drink_id"], name: "index_ratings_on_drink_id"
+    t.index ["user_id"], name: "index_ratings_on_user_id"
   end
 
-  create_table "users", id: :bigint, default: nil, force: :cascade do |t|
-    t.text "email", default: ""
-    t.text "encrypted_password", default: ""
-    t.text "reset_password_token"
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.boolean "admin_access", default: false
-    t.text "provider"
-    t.text "uid"
-    t.text "image"
-    t.index ["email"], name: "idx_16436_index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "idx_16436_index_users_on_reset_password_token", unique: true
+    t.string "provider"
+    t.string "uid"
+    t.string "image"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "ingredients", "flavor_profiles", name: "ingredients_flavor_profile_id_fkey"
-  add_foreign_key "quiz_ratings", "drinks", name: "quiz_ratings_drink_id_fkey"
-  add_foreign_key "quiz_ratings", "quizzes", name: "quiz_ratings_quiz_id_fkey"
-  add_foreign_key "quizzes", "users", name: "quizzes_user_id_fkey"
-  add_foreign_key "ratings", "drinks", name: "ratings_drink_id_fkey"
-  add_foreign_key "ratings", "users", name: "ratings_user_id_fkey"
+  add_foreign_key "ingredients", "flavor_profiles"
+  add_foreign_key "measures", "drinks"
+  add_foreign_key "measures", "ingredients"
+  add_foreign_key "quiz_ratings", "drinks"
+  add_foreign_key "quiz_ratings", "quizzes"
+  add_foreign_key "quizzes", "users"
+  add_foreign_key "ratings", "drinks"
+  add_foreign_key "ratings", "users"
 end
