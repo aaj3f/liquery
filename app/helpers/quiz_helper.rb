307e31e7b3aa2fbf7f0ebb8b1aaf205057785a4b
Andrew Johnson <ajohnson.uva@gmail.com>
2019-04-22 19:15:48 -0400

    adjust innertext for responsive sizing

--- app/helpers/quiz_helper.rb
+++ app/helpers/quiz_helper.rb
@@ -2,17 +2,17 @@ module QuizHelper
 
   def modal_header
     if current_user.ratings.present?
-      "Yasss boo!! Welcome back!!"
+      "Yasss boo!!<br>Welcome back!!"
     else
-      "Heyo sis! You look thirsty!"
+      "Heyo sis!<br>You look thirsty!"
     end
   end
 
   def modal_text
     if current_user.ratings.present?
-      "We already have a sense for some of the drinks you tend to like. But if you wanna give us some new information or update your likes and dislikes, go ahead bebe!!"
+      "We already have a sense for some of the drinks you tend to like.<br>But if you wanna give us some new information or update your likes and dislikes<br>go ahead bebe!!"
     else
-      "We're gonna get you a cocktail pronto, we just need to ask a few questions first so we can figure your drink out."
+      "We're gonna get you a cocktail pronto,<br>we just need to ask a few questions first<br>so we can figure your drink out."
     end
   end
 
