ef3d554c18120cc1e8a86be46a6efd201cd56e16
Andrew Johnson <ajohnson.uva@gmail.com>
2019-04-22 15:25:51 -0400

    add privacy policy to web app

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
   get 'welcome/index'
   root 'welcome#index'
 
+  get 'privacy', to: 'privacy#show'
+
   # resources :admin do
   #   resources :drinks, only %i(new)
   # end
