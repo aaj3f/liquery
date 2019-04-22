e7e72f661f1112e7b6a5a9823b407823c873fd7a
Andrew Johnson <ajohnson.uva@gmail.com>
2019-04-22 19:42:46 -0400

    sanitize helper strings

--- app/helpers/quiz_helper.rb
+++ app/helpers/quiz_helper.rb
@@ -2,17 +2,17 @@ module QuizHelper
 
   def modal_header
     if current_user.ratings.present?
-      'Yasss boo!!' + tag(:br) + 'Welcome back!!'
+      'Yasss boo!!<br>Welcome back!!'.sanitize
     else
-      'Heyo sis!' + tag(:br) + 'You look thirsty!'
+      'Heyo sis!<br>You look thirsty!'.sanitize
     end
   end
 
   def modal_text
     if current_user.ratings.present?
-      'We already have a sense for some of the drinks you tend to like.' + tag(:br) + 'But if you wanna give us some new information or update your likes and dislikes' + tag(:br) + 'go ahead bebe!!'
+      'We already have a sense for some of the drinks you tend to like.<br>But if you wanna give us some new information or update your likes and dislikes<br>go ahead bebe!!'.sanitize
     else
-      'We\'re gonna get you a cocktail pronto,' + tag(:br) + 'we just need to ask a few questions first' + tag(:br) + 'so we can figure your drink out.'
+      'We\'re gonna get you a cocktail pronto,<br>we just need to ask a few questions first<br>so we can figure your drink out.'.sanitize
     end
   end
 
