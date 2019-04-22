98b96522d93266dbdb0be4e3bbbd3eb738c0673b
Andrew Johnson <ajohnson.uva@gmail.com>
2019-04-22 19:27:07 -0400

    add rails content tags

--- app/helpers/quiz_helper.rb
+++ app/helpers/quiz_helper.rb
@@ -2,17 +2,17 @@ module QuizHelper
 
   def modal_header
     if current_user.ratings.present?
-      CGI::escapeHTML('Yasss boo!!<br>Welcome back!!')
+      'Yasss boo!!' + tag(:br) + 'Welcome back!!'
     else
-      CGI::escapeHTML('Heyo sis!<br>You look thirsty!')
+      'Heyo sis!' + tag(:br) + 'You look thirsty!'
     end
   end
 
   def modal_text
     if current_user.ratings.present?
-      CGI::escapeHTML('We already have a sense for some of the drinks you tend to like.<br>But if you wanna give us some new information or update your likes and dislikes<br>go ahead bebe!!')
+      'We already have a sense for some of the drinks you tend to like.' + tag(:br) + 'But if you wanna give us some new information or update your likes and dislikes' + tag(:br) + 'go ahead bebe!!'
     else
-      CGI::escapeHTML('We\'re gonna get you a cocktail pronto,<br>we just need to ask a few questions first<br>so we can figure your drink out.')
+      'We\'re gonna get you a cocktail pronto,' + tag(:br) + 'we just need to ask a few questions first' + tag(:br) + 'so we can figure your drink out.'
     end
   end
 
