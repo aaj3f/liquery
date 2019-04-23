b1f7b60f89ebd66e2e41132c9a1e7702244b5340
Andrew Johnson <ajohnson.uva@gmail.com>
2019-04-22 20:02:26 -0400

    call html_safe on strings in helper

--- app/helpers/quiz_helper.rb
+++ app/helpers/quiz_helper.rb
@@ -2,17 +2,17 @@ module QuizHelper
 
   def modal_header
     if current_user.ratings.present?
-      'Yasss boo!!<br>Welcome back!!'.sanitize
+      'Yasss boo!!<br>Welcome back!!'.html_safe
     else
-      'Heyo sis!<br>You look thirsty!'.sanitize
+      'Heyo sis!<br>You look thirsty!'.html_safe
     end
   end
 
   def modal_text
     if current_user.ratings.present?
-      'We already have a sense for some of the drinks you tend to like.<br>But if you wanna give us some new information or update your likes and dislikes<br>go ahead bebe!!'.sanitize
+      'We already have a sense for some of the drinks you tend to like.<br>But if you wanna give us some new information or update your likes and dislikes<br>go ahead bebe!!'.html_safe
     else
-      'We\'re gonna get you a cocktail pronto,<br>we just need to ask a few questions first<br>so we can figure your drink out.'.sanitize
+      'We\'re gonna get you a cocktail pronto,<br>we just need to ask a few questions first<br>so we can figure your drink out.'.html_safe
     end
   end
 
