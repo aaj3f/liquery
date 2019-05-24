88ba378442bc3fea89868804e48e70a7c2cee4b0
Andrew Johnson <ajohnson.uva@gmail.com>
2019-05-24 10:46:43 -0400

    edit flavor text

--- app/helpers/quiz_helper.rb
+++ app/helpers/quiz_helper.rb
@@ -2,15 +2,15 @@ module QuizHelper
 
   def modal_header
     if current_user.ratings.present?
-      'Yasss boo!! Welcome back!!'
+      'We\'ve met before! Welcome back!'
     else
-      'Heyo sis! You look thirsty!'
+      'Hey there! You look thirsty!'
     end
   end
 
   def modal_text
     if current_user.ratings.present?
-      'We already have a sense for some of the drinks you tend to like. But if you wanna give us some new information or update your likes and dislikes go ahead bebe!!'
+      'We already have a sense for some of the drinks you tend to like. But if you wanna give us some new information or update your likes and dislikes, go ahead!!'
     else
       'We\'re gonna get you a cocktail pronto, we just need to ask a few questions first so we can figure your drink out.'
     end
